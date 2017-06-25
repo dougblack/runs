8d0153d37f52099908858b6ca5e8cc812cb4afcc
Doug Black <dblack@twilio.com>
2017-06-24 21:11:29 -0700

    Refactor into header function

--- cli/month.go
+++ cli/month.go
@@ -42,18 +42,23 @@ func total(now time.Time, date time.Time, runs []data.Run) float64 {
 	return total
 }
 
-func printRuns(runs []data.Run) {
-	const headerWidth = 27
-	now := time.Now().Local()
-	totalDays := time.Date(now.Year(), now.Month() + 1, 0, 0, 0, 0, 0, now.Location()).Day()
+func header(now time.Time) {
+	const headerWidth = 26
 	headerMonth := fmt.Sprintf("%s %d", now.Month().String(), now.Year())
 	leftPadding := (headerWidth - len(headerMonth)) / 2
 	padding := strings.Repeat(" ", leftPadding)
 	fmt.Printf("%s%s\n", padding, headerMonth)
 	fmt.Println("Su Mo Tu We Th Fr Sa Tt")
+}
 
+func printRuns(runs []data.Run) {
+	now := time.Now().Local()
+	header(now)
+
+	totalDays := time.Date(now.Year(), now.Month() + 1, 0, 0, 0, 0, 0, now.Location()).Day()
 	first := time.Date(now.Year(), now.Month(), 1, 1, 0, 0, 0, now.Location())
 	last := time.Date(now.Year(), now.Month() + 1, 0, 0, 0, 0, 0, now.Location())
+
 	for i := 0; i < int(first.Weekday()); i++ {
 		print("   ")
 	}
@@ -66,7 +71,7 @@ func printRuns(runs []data.Run) {
 		if dayTotal > 0 {
 			fmt.Printf("%2d", int(dayTotal))
 		} else {
-			fmt.Printf("--")
+			fmt.Printf("  ")
 		}
 		fmt.Print(" ")
 		if date.Weekday() == time.Saturday {
@@ -77,14 +82,12 @@ func printRuns(runs []data.Run) {
 
 	for i := int(last.Weekday()); i <= int(time.Sunday); i++ {
 		date := time.Date(now.Year(), now.Month() + 1, i, 0, 0, 0, 0, now.Location())
-		fmt.Printf("--")
+		fmt.Printf("  ")
 		fmt.Print(" ")
 		if date.Weekday() == time.Saturday {
 			fmt.Printf(" %3.1f\n", weekTotal)
 		}
 	}
-
-	fmt.Print("\n")
 }
 
 func (m *MonthCommand) Execute(_ context.Context, f *flag.FlagSet, _ ...interface{}) subcommands.ExitStatus {
